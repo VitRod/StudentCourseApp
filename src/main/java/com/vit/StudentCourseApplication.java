f79e9d7d031512c4c7767199dfdcee82393d7d28
Vitaliy Rodyna <ukrdao@gmail.com>
2022-05-14 14:02:33 +0300

    added first files  of  project

---
+++ src/main/java/com/vit/StudentCourseApplication.java
@@ -0,0 +1,17 @@
+package com.vit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class StudentCourseApplication {
+
+	public static void main(String[] args) {
+		ApplicationContext ctx =  SpringApplication.run(
+				StudentCourseApplication.class, args);
+		//System.out.println(ctx);
+	}
+
+}
+ 
---
+++ src/test/java/com/vit/StudentCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
